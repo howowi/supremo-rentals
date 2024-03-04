9a3e8e1350846acc9cdebe2d4daafd7932e26127
howie.owi@oracle.com <owiweihow@gmail.com>
2024-03-04 16:22:39 +0800

    updated backend IP

--- k6/protocol_loadtest_arm.js
+++ k6/protocol_loadtest_arm.js
@@ -21,34 +21,34 @@ export async function browserTest() {
   let response
 
   group('page_2 - http://supremorentalsarm.oracledemo.online/cardetails/t001', function () {
-    response = http.get('http://140.238.167.80:5000/car-service-redis/cars/t001')
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
-    response = http.get('http://140.238.167.80:5000/user-service-redis/users/JohnC')
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/car-service-redis/cars/t001')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/user-service-redis/users/JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
     sleep(1.8)
   })
 
   group('page_3 - http://supremorentalsarm.oracledemo.online/carcheckout/t001', function () {
-    response = http.get('http://140.238.167.80:5000/car-service-redis/cars/t001')
+    response = http.get('http://152.67.25.167/car-service-redis/cars/t001')
 
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
 
-    response = http.get('http://140.238.167.80:5000/user-service-redis/users/JohnC')
+    response = http.get('http://152.67.25.167/user-service-redis/users/JohnC')
 
-    response = http.get('http://140.238.167.80:5000/user-service-redis/users/JohnC')
+    response = http.get('http://152.67.25.167/user-service-redis/users/JohnC')
 
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
 
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
 
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
 
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
     sleep(1.1)
 
     response = http.post(
-      'http://140.238.167.80:5000/order-service/create-order',
+      'http://152.67.25.167/order-service/create-order',
       '{"userid":"JohnC","carid":"t001","brand":"Toyoto","name":"Corolla","from_date":"2024-02-29","end_date":"2024-03-01","duration":1,"ordered":"TRUE"}',
       {
         headers: {
@@ -57,7 +57,7 @@ export async function browserTest() {
       }
     )
 
-    response = http.options('http://140.238.167.80:5000/order-service/create-order', null, {
+    response = http.options('http://152.67.25.167/order-service/create-order', null, {
       headers: {
         accept: '*/*',
         'access-control-request-headers': 'content-type',
@@ -69,8 +69,8 @@ export async function browserTest() {
   })
 
   group('page_4 - http://supremorentalsarm.oracledemo.online/confirmbooking/t001', function () {
-    response = http.get('http://140.238.167.80:5000/car-service-redis/cars/t001')
-    response = http.get('http://140.238.167.80:5000/user-service-redis/users/JohnC')
-    response = http.get('http://140.238.167.80:5000/order-service/user-orders?userid=JohnC')
+    response = http.get('http://152.67.25.167/car-service-redis/cars/t001')
+    response = http.get('http://152.67.25.167/user-service-redis/users/JohnC')
+    response = http.get('http://152.67.25.167/order-service/user-orders?userid=JohnC')
   })
 }
